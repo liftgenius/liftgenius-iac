249cd778f36c0280b0c30512c8bdc57025fd7714
John Nevin <john@liftgeni.us>
2026-04-09 14:01:24 -0400

    Update AWS provider
    Use variable for port.

--- environments/local/provider.tf
+++ environments/local/provider.tf
@@ -2,34 +2,35 @@ provider "aws" {
   access_key                  = var.aws_access_key
   secret_key                  = var.aws_secret_key
   region                      = var.aws_region
-  s3_use_path_style           = true
+  s3_use_path_style = true
   skip_credentials_validation = true
-  skip_metadata_api_check     = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
 
   endpoints {
-    # apigateway     = "http://${var.localstack_ip}:4566"
-    # apigatewayv2   = "http://${var.localstack_ip}:4566"
-    # cloudformation = "http://${var.localstack_ip}:4566"
-    # cloudwatch     = "http://${var.localstack_ip}:4566"
-    # dynamodb       = "http://${var.localstack_ip}:4566"
-    ec2            = "http://${var.localstack_host}:${var.localstack_port}"
-    # es             = "http://${var.localstack_ip}:4566"
-    # elasticache    = "http://${var.localstack_ip}:4566"
-    # firehose       = "http://${var.localstack_ip}:4566"
-    iam            = "http://${var.localstack_host}:4566"
-    # kinesis        = "http://${var.localstack_ip}:4566"
-    lambda         = "http://${var.localstack_host}:4566"
-    # rds            = "http://${var.localstack_ip}:4566"
-    # redshift       = "http://${var.localstack_ip}:4566"
-    # route53        = "http://${var.localstack_ip}:4566"
-    s3             = "http://s3.localhost.localstack.cloud:4566" # needs to be set to localhost instead of container ip
-    # secretsmanager = "http://${var.localstack_ip}:4566"
-    # ses            = "http://${var.localstack_ip}:4566"
-    # sns            = "http://${var.localstack_ip}:4566"
-    # sqs            = "http://${var.localstack_ip}:4566"
-    # ssm            = "http://${var.localstack_ip}:4566"
-    # stepfunctions  = "http://${var.localstack_ip}:4566"
-    sts            = "http://${var.localstack_host}:4566"
+    # apigateway     = "http://${var.localstack_ip}:${var.localstack_port}"
+    # apigatewayv2   = "http://${var.localstack_ip}:${var.localstack_port}"
+    # cloudformation = "http://${var.localstack_ip}:${var.localstack_port}"
+    # cloudwatch     = "http://${var.localstack_ip}:${var.localstack_port}"
+    # dynamodb       = "http://${var.localstack_ip}:${var.localstack_port}"
+    # ec2            = "http://${var.localstack_host}:${var.localstack_port}"
+    # es             = "http://${var.localstack_ip}:${var.localstack_port}"
+    # elasticache    = "http://${var.localstack_ip}:${var.localstack_port}"
+    # firehose       = "http://${var.localstack_ip}:${var.localstack_port}"
+    iam            = "http://${var.localstack_host}:${var.localstack_port}"
+    # kinesis        = "http://${var.localstack_ip}:${var.localstack_port}"
+    lambda         = "http://${var.localstack_host}:${var.localstack_port}"
+    # rds            = "http://${var.localstack_ip}:${var.localstack_port}"
+    # redshift       = "http://${var.localstack_ip}:${var.localstack_port}"
+    # route53        = "http://${var.localstack_ip}:${var.localstack_port}"
+    s3             = "http://${var.localstack_host}:${var.localstack_port}"
+    # secretsmanager = "http://${var.localstack_ip}:${var.localstack_port}"
+    # ses            = "http://${var.localstack_ip}:${var.localstack_port}"
+    # sns            = "http://${var.localstack_ip}:${var.localstack_port}"
+    # sqs            = "http://${var.localstack_ip}:${var.localstack_port}"
+    # ssm            = "http://${var.localstack_ip}:${var.localstack_port}"
+    # stepfunctions  = "http://${var.localstack_ip}:${var.localstack_port}"
+    # sts            = "http://${var.localstack_host}:${var.localstack_port}"
   }
 }
 
